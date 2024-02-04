ea43e7ee8131eb522ce3c801fb45817b1de319e0
Kaustab Samanta <kou8481@gmail.com>
2024-02-04 19:50:41 +0530

    Multiplt assets added, dark mode added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  public isDarkMode = false;
 
+  public toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    document.documentElement.classList.toggle('dark-mode');
+  }
 }
