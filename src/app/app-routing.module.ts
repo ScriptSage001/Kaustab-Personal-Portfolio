216fc825574dac9ea7abff0119740bf646f0bcfe
Kaustab Samanta <kou8481@gmail.com>
2024-02-14 10:42:36 +0530

    Fixing font related issues

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { HomeNewComponent } from './home-new/home-new.component';
 
 const routes: Routes = [
   { path: '', component: HomeNewComponent },
-  { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent }
 ];
 
